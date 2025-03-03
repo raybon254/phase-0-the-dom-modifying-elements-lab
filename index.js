3ed7d541b11c205e408c9d3209bf3124f7c4ef89
Brian <musyimib449@gmail.com>
2025-03-03 16:10:16 +0300

    modifying elements in lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Brian is the champion";
+
+document.body.appendChild(newHeader);
